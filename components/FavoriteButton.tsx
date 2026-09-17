815b432073f054198b681b58371c8420cca646d4
mjm29 <MarcJoseph.Millare@edu.sait.ca>
2026-09-16 22:37:50 -0600

    Created Favorite Button

--- components/FavoriteButton.tsx
+++ components/FavoriteButton.tsx
@@ -1,11 +1,24 @@
 import { useState } from "react";
-
-const [favorited, setFavorited] = useState(false);
-
-function handleFavorite() {
-  setFavorited(true);
-}
+import { Text, Pressable, StyleSheet } from 'react-native';
 
 export default function FavoriteButton() {
-  
+
+    const [favorited, setFavorited] = useState(false);
+
+  return (  
+    <Pressable style={styles.button} onPress={() => setFavorited(!favorited)} hitSlop={10}>
+      <Text>{favorited ? '❤️' : '🤍'}</Text>
+    </Pressable>
+  );
 }
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: 'rgb(0,0,0)',
+    borderRadius: 16,
+    width: 32,
+    height: 32,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
